dec02bfc3834cef995d03a3a09988deaf7412a03
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-08 09:52:01 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,1 +1,14 @@
-chrome.runtime.onInstalled.addListener(()=>chrome.contextMenus.create({id:"",title:"Copy textContent",contexts:["page","link"]})),chrome.contextMenus.onClicked.addListener((a,b)=>b.url[0]!="c"&&chrome.scripting.executeScript({target:(a=a.frameId)?{tabId:b.id,frameIds:[a]}:{tabId:b.id},world:"MAIN",func:()=>navigator.clipboard.writeText(document.activeElement.textContent)}))
+chrome.runtime.onInstalled.addListener(()=>
+  chrome.contextMenus.create({
+    id: "",
+    title: "Copy textContent",
+    contexts:["page","link"]
+  })
+)
+chrome.contextMenus.onClicked.addListener((a, b)=>
+  b.url[0] != "c" && chrome.scripting.executeScript({
+    target: (a = a.frameId) ? {tabId: b.id, frameIds:[a]} : {tabId: b.id},
+    world: "MAIN",
+    func: ()=> navigator.clipboard.writeText(document.activeElement.textContent)
+  })
+)
