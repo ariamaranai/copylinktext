e7dee301a9012c5234543730295241ce2c0027fc
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-08 09:51:15 +0900

    Rename s.js to bg.js

---
+++ bg.js
@@ -0,0 +1,1 @@
+chrome.runtime.onInstalled.addListener(()=>chrome.contextMenus.create({id:"",title:"Copy textContent",contexts:["page","link"]})),chrome.contextMenus.onClicked.addListener((a,b)=>b.url[0]!="c"&&chrome.scripting.executeScript({target:(a=a.frameId)?{tabId:b.id,frameIds:[a]}:{tabId:b.id},world:"MAIN",func:()=>navigator.clipboard.writeText(document.activeElement.textContent)}))
