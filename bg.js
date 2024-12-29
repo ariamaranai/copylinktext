9164247261e0dc5c7ab2f9a6267e35b74135b3fb
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-29 10:25:32 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -2,26 +2,24 @@ chrome.runtime.onInstalled.addListener(() =>
   chrome.contextMenus.create({
     id: "",
     title: "Copy text",
-    contexts: ["page", "link"]
+    contexts: ["page", "link"],
+    documentUrlPatterns: ["https://*/*", "https://*/", "http://*/*", "http://*/"]
   })
 );
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (tab.url[0] != "c") {
-    let frameId = info.frameId;
-    chrome.scripting.executeScript({
-      target: frameId
-        ? { tabId: tab.id, frameIds: [frameId] }
-        : { tabId: tab.id },
-      world: "MAIN",
-      func: () => {
-        let activeElement = document.activeElement;
-        let selection = getSelection();
-        let range = document.createRange();
-        selection.removeAllRanges();
-        range.selectNodeContents(activeElement);
-        selection.addRange(range);
-        navigator.clipboard.writeText(activeElement.textContent);
-      }
-    });
-  }
-});
+chrome.contextMenus.onClicked.addListener((info, tab) =>
+  chrome.scripting.executeScript({
+    target: info.frameId
+      ? { tabId: tab.id, frameIds: [info.frameId] }
+      : { tabId: tab.id },
+    world: "MAIN",
+    func: () => {
+      let activeElement = document.activeElement;
+      let selection = getSelection();
+      let range = document.createRange();
+      selection.removeAllRanges();
+      range.selectNodeContents(activeElement);
+      selection.addRange(range);
+      navigator.clipboard.writeText(activeElement.textContent);
+    }
+  })
+);
