1d1872adaa4a2104ebd8a5abd35dd1f0ef0d4e9e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-03 13:31:45 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,12 +6,14 @@ chrome.runtime.onInstalled.addListener(() =>
     documentUrlPatterns: ["https://*/*", "https://*/", "http://*/*", "http://*/", "file://*/*", "file://*/"]
   })
 );
-chrome.contextMenus.onClicked.addListener((info, tab) =>
+chrome.contextMenus.onClicked.addListener(async (info, tab) =>
   chrome.scripting.executeScript({
     target: info.frameId
       ? { tabId: tab.id, frameIds: [info.frameId] }
       : { tabId: tab.id },
-    world: "MAIN",
+    world: (await chrome.contentSettings.javascript.get({
+      primaryUrl: tab.url
+    })).setting == "allow" ? "MAIN" : "ISOLATED",
     func: () => {
       let activeElement = document.activeElement;
       let selection = getSelection();
