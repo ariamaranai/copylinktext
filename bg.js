b423c8e32166104918d75ea983b1fed64025f0d6
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-03 22:45:03 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,23 +6,25 @@ chrome.runtime.onInstalled.addListener(() =>
     documentUrlPatterns: ["https://*/*", "https://*/", "http://*/*", "http://*/", "file://*/*", "file://*/"]
   })
 );
-chrome.contextMenus.onClicked.addListener(async (info, tab) =>
-  chrome.scripting.executeScript({
-    target: info.frameId
-      ? { tabId: tab.id, frameIds: [info.frameId] }
-      : { tabId: tab.id },
-    world: (await chrome.contentSettings.javascript.get({
-      primaryUrl: tab.url
-    })).setting == "allow" ? "MAIN" : "ISOLATED",
-    func: () => {
-      let d = document;
-      let activeElement = d.activeElement;
-      let selection = getSelection();
-      let range = d.createRange();
-      selection.removeAllRanges();
-      range.selectNodeContents(activeElement);
-      selection.addRange(range);
-      navigator.clipboard.writeText(activeElement.textContent);
-    }
-  })
-);
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  try {
+    chrome.scripting.executeScript({
+      target: info.frameId
+        ? { tabId: tab.id, frameIds: [info.frameId] }
+        : { tabId: tab.id },
+      world: (await chrome.contentSettings.javascript.get({
+        primaryUrl: tab.url
+      })).setting == "allow" ? "MAIN" : "ISOLATED",
+      func: () => {
+        let d = document;
+        let activeElement = d.activeElement;
+        let selection = getSelection();
+        let range = d.createRange();
+        selection.removeAllRanges();
+        range.selectNodeContents(activeElement);
+        selection.addRange(range);
+        navigator.clipboard.writeText(activeElement.textContent);
+      }
+    })
+  } catch (e) {}
+});
