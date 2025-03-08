93eb18df0f3b3b600845213933ef8a5dd8ff3867
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-08 13:52:25 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,21 +1,18 @@
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  try {
-    let tabId = tab.id;
-    chrome.scripting.executeScript({
-      target: info.frameId ? { tabId, frameIds: [info.frameId] } : { tabId },
-      func: () => {
-        let d = document;
-        let activeElement = d.activeElement;
-        let selection = getSelection();
-        let range = d.createRange();
-        selection.removeAllRanges();
-        range.selectNodeContents(activeElement);
-        selection.addRange(range);
-        navigator.clipboard.writeText(activeElement.textContent);
-      }
-    });
-  } catch (e) {}
-});
+chrome.contextMenus.onClicked.addListener((info, { id: tabId }) =>
+  chrome.scripting.executeScript({
+    target: info.frameId ? { tabId, frameIds: [info.frameId] } : { tabId },
+    func: () => {
+      let d = document;
+      let activeElement = d.activeElement;
+      let selection = getSelection();
+      let range = d.createRange();
+      selection.removeAllRanges();
+      range.selectNodeContents(activeElement);
+      selection.addRange(range);
+      navigator.clipboard.writeText(activeElement.textContent);
+    }
+  }).catch(() => 0)
+);
 chrome.runtime.onInstalled.addListener(() =>
   chrome.contextMenus.create({
     id: "",
