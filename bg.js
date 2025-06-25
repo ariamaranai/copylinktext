7d8f5e604812ff114f8fcf90a8fdf2111347dcd8
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 21:49:34 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,9 +1,11 @@
-chrome.contextMenus.onClicked.addListener(({ frameId }, { id: tabId }) =>
-  chrome.userScripts.execute({
-    target: frameId ? { tabId, frameIds: [frameId] } : { tabId },
-    js: [{ code: "{let d=document,e=d.activeElement,s=getSelection(),r=d.createRange(s.removeAllRanges());r.selectNodeContents(e),s.addRange(r),navigator.clipboard.writeText(e.textContent)}" }]
-  }).catch(() => 0)
-);
+chrome.contextMenus.onClicked.addListener(async ({ frameId }, { id: tabId }) => {
+  try {
+    await chrome.userScripts.execute({
+      target: frameId ? { tabId, frameIds: [frameId] } : { tabId },
+      js: [{ code: "{let d=document,e=d.activeElement,s=getSelection(),r=d.createRange(s.removeAllRanges());r.selectNodeContents(e),s.addRange(r),navigator.clipboard.writeText(e.textContent)}" }]
+    })
+  } catch (e) {}
+});
 chrome.runtime.onInstalled.addListener(() =>
   chrome.contextMenus.create({
     id: "",
