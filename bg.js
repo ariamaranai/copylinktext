a0cf7ac72d29ee818d097a0487e1c6e77a0bc766
a <106607480+ariamaranai@users.noreply.github.com>
2026-03-21 08:10:51 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -2,9 +2,18 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   try {
     let tabId = tab.id;
     let { frameId } = info;
-    await chrome.userScripts.execute({
+    await chrome.scripting.executeScript({
       target: frameId ? { tabId, frameIds: [frameId] } : { tabId },
-      js: [{ code: "{let d=document,e=d.activeElement,s=getSelection(),r=d.createRange(s.removeAllRanges());r.selectNodeContents(e),s.addRange(r),d.execCommand('copy')}" }]
+      world: "MAIN",
+      func: () => {
+        let d = document;
+        let e = d.activeElement;
+        let s = getSelection();
+        let r = d.createRange(s.removeAllRanges());
+        r.selectNodeContents(e);
+        s.addRange(r);
+        d.execCommand("copy")
+      }
     })
   } catch {}
 });
